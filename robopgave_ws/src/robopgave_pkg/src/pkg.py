d3eeff119751f11b490600e4a86083a5e65ca662
MrVolans <mahansen99@gmail.com>
2021-11-12 12:14:01 +0100

    test

--- robopgave_ws/src/robopgave_pkg/src/pkg.py
+++ robopgave_ws/src/robopgave_pkg/src/pkg.py
@@ -2,6 +2,10 @@
 import rospy
 from std_msgs.msg import String
 
+import cv2 as cv
+
+
+
 def talker():
     pub = rospy.Publisher("chatter", String, queue_size=10)
     rospy.init_node("talker", anonymous=True)
