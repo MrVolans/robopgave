d36b52515b1fcf2ce9190ba47e01c1a6a7c594c5
Vodkashotski <siwad19@student.sdu.dk>
2021-11-16 11:38:02 +0100

    yo

--- robopgave_ws/devel/share/robopgave_pkg/cmake/robopgave_pkg-msg-extras.cmake
+++ robopgave_ws/devel/share/robopgave_pkg/cmake/robopgave_pkg-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(robopgave_pkg_MESSAGE_FILES "/home/simon/ROB5/robopgave/robopgave_ws/src/robopgave_pkg/msg/Num.msg")
-set(robopgave_pkg_SERVICE_FILES "")
+set(robopgave_pkg_SERVICE_FILES "/home/simon/ROB5/robopgave/robopgave_ws/src/robopgave_pkg/srv/AddTwoInts.srv")
---
+++ robopgave_ws/install/share/common-lisp/ros/robopgave_pkg/srv/_package.lisp
@@ -0,0 +1,10 @@
+(cl:defpackage robopgave_pkg-srv
+  (:use )
+  (:export
+   "ADDTWOINTS"
+   "<ADDTWOINTS-REQUEST>"
+   "ADDTWOINTS-REQUEST"
+   "<ADDTWOINTS-RESPONSE>"
+   "ADDTWOINTS-RESPONSE"
+  ))
+
