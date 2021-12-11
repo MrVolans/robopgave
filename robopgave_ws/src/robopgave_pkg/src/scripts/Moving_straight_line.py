b3e5cee49f8fcd461a55437a0d163f5d88fcd1c2
MrVolans <mahansen99@gmail.com>
2021-12-11 10:35:58 +0100

    just some stuff

--- robopgave_ws/src/robopgave_pkg/src/scripts/Moving_straight_line.py
+++ robopgave_ws/src/robopgave_pkg/src/scripts/Moving_straight_line.py
@@ -7,10 +7,11 @@ from geometry_msgs.msg import Twist
 from rospy.topics import Publisher
 from turtlesim.msg import Pose
 
-
-
-
-
+DISTANCE_EPSILON = 0.1
+ANGLE_EPSILON = 0.01
+position = np.array([0,0])
+speeddiv = 0.15
+xUnit = np.array([1, 0])
 
 def move():
     #Receiveing the user's input
@@ -18,26 +19,40 @@ def move():
     speed = int(input("Input your speed: "))
     vel_msg = Twist()
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    x, y = [ int(x) for x in input("Type your coordinateas x, y :").split(", ")]
+    destination = np.array([int(x) for x in input("Type your coordinateas x, y :").split(", ")])
     def callback(data):
+        position = np.array([data.x, data.y])
         rospy.loginfo(rospy.get_caller_id() + 'Pose is %s', data.data) 
-
+    
     def angular_pose_callback(data):
-        angle = math.acos(x/math.sqrt(x**2 + y**2))
-        if int(100*angle)/100 != int(100*data.theta)/100 :
-            vel_msg.angular.z = 0.1*speed* np.sign(angle-data.theta)
+
+        distance = destination-position
+        if np.linalg.norm(distance) < DISTANCE_EPSILON :
+            print("Too short")
+            return
+        direction = distance/np.linalg.norm(distance)
+        rotationAngle = math.acos(np.dot(xUnit, direction))+data.theta
+        if abs(rotationAngle) > ANGLE_EPSILON:
+            vel_msg.angular.z = np.sign(rotationAngle)*speed*speeddiv
             velocity_publisher.publish(vel_msg)
-            rospy.loginfo(rospy.get_caller_id() + "Angule is %s", int(100*angle)/100)
-            rospy.loginfo(rospy.get_caller_id() + "Theta is %s", int(100*data.theta)/100)
+            rospy.loginfo(rospy.get_caller_id() + " Angule is %s", rotationAngle)
+            rospy.loginfo(rospy.get_caller_id().split("_")[0] + " Theta is %s", int(100*data.theta)/100)
         else:
             vel_msg.angular.z = 0
             velocity_publisher.publish(vel_msg)
             linear_pose_callback(data)
 
     def linear_pose_callback(data):
-        vel_msg.linear.x = speed
-        velocity_publisher.publish(vel_msg)
-        print("pront")
+        position = np.array([data.x, data.y])
+        distance_To_Goal = np.linalg.norm(destination-position)
+        rospy.loginfo(rospy.get_caller_id().split("_")[0] + " Distance is: %s", distance_To_Goal)
+        if distance_To_Goal < 0.1 :
+            vel_msg.linear.x = 0
+            rospy.loginfo("arrived at %s", position)
+        else :
+            vel_msg.linear.x = speed/4
+            velocity_publisher.publish(vel_msg)
+        
 
     rospy.init_node("Jens", anonymous=True)
     pose_subscriber = rospy.Subscriber("/turtle1/pose", Pose, angular_pose_callback)
